dcaef7c71b4e24c4b8578005748482fcc36fda34
Mohammad el kaswani <mohammad-kiswani@outlook.com>
2020-03-12 18:23:11 -0400

    Questin3

---
+++ Question3.java
@@ -0,0 +1,29 @@
+/*
+program that prompts the user for the current temperature in Celsius
+and outputs different messages if it is above or below zero.
+
+Written By MOHAMMAD EL KASWANI
+*/
+import java.util.*;
+
+public class Quetion3{
+
+    public static void main (String[] args){
+
+        //declaring variables
+        double temperature;
+        Scanner sc = new Scanner(System.in);
+
+        //getting user input
+        System.out.println("\nWhat is the current temperature?");
+        temperature = sc.nextDouble();
+
+        //Printing
+        if(temperature <= 0){
+            System.out.println("\nThe temperature is below zero");
+        }else if(temperature >= 0){
+            System.out.println("\nThe temperatureis above the zero");
+        }
+    }
+
+}
